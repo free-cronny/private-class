763e39821ea592c17d2c1d3e48161887335d4d8a
DESKTOP-FKDJ4EE\NÃO MEXA NUNCA <cawancnd@gmail.com>
2023-12-12 21:55:20 -0300

    fix: Alterações no Projeto

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,9 @@
-import './App.css'
-
+import { Button } from "./components/Button"
 function App() {
-
-  const apiKey = process.env.REACT_APP_API_KEY;
-  const apiUrl = process.env.REACT_APP_API_URL;
-  
-  console.log('Essa é a api usando a .env para organizar o projeto', apiKey)
-  console.log('Essa é a api usando a .env para organizar o projeto', apiUrl)
 
   return (
     <>
-      <p>{apiKey} : {apiUrl}</p>
+      <Button title="Login"/>
     </>
   )
 }
---
+++ src/utils/firebase.js
@@ -0,0 +1,11 @@
+// const apiKey = import.meta.env.VITE_API_KEY;
+// const apiUrl = import.meta.env.VITE_API_URL;
+// const authDomain = import.meta.env.VITE_API_DOMAIN;
+// const firebaseConfig = {
+//   apiKey: {apiKey},
+//   authDomain: {authDomain},
+//   projectId: "private-class-ddfa5",
+//   storageBucket: "private-class-ddfa5.appspot.com",
+//   messagingSenderId: "699454608493",
+//   appId: "1:699454608493:web:828951a04de1d288755b64"
+// };

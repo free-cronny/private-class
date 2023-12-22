76e036c81b04f65b3034b45767d308294c970352
IgorMarines <igor.marines25@gmail.com>
2023-12-22 08:51:37 -0300

    fix: layout responsivo sidebar

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 // Importações omitidas para brevidade
 import * as S from './styles';
 import * as G from '../../styles/styles';
@@ -8,12 +9,67 @@ import Avatar1 from '../../assets/images/pessoaAvatar.jpg';
 import Avatar2 from '../../assets/images/pessoaAvatar02.jpg';
 import Avatar3 from '../../assets/images/pessoaAvatar03.jpg';
 
+import { FiMenu } from 'react-icons/fi'
+
 import { PiNewspaperClippingFill } from "react-icons/pi";
 
+import { useState, useEffect } from 'react';
+import useAuthStore from "../../utils/store";
+import { getDatabase, onValue, ref } from 'firebase/database';
+import { useMediaQuery } from '@mui/material';
+
 export const Home = () => {
+
+  const [userEmails, setUserEmails] = useState([]);
+    const { isAuthenticated } = useAuthStore();
+    const isOnDesktop = useMediaQuery('(min-width:600px)');
+    const [sidebarOpen, setSidebarOpen] = useState(true);
+
+  useEffect(() => {
+    if (isAuthenticated) {
+        const usersRef = ref(getDatabase(), 'users');
+        onValue(usersRef, (snapshot) => {
+            try {
+                if (snapshot.exists()) {
+                    const usersData = snapshot.val();
+                    // Verifique se há dados e mapeie os emails
+                    const emails = Object.values(usersData).map((user) => user);
+                    setUserEmails(emails);
+                } else {
+                    // Caso não haja dados
+                    setUserEmails([]);
+                }
+            } catch (error) {
+                console.error("Erro ao processar dados do Realtime Database:", error);
+                // Trate o erro conforme necessário
+            }
+        }, (error) => {
+            console.error("Erro ao buscar dados do Realtime Database:", error);
+            // Trate o erro conforme necessário
+        });
+    }
+}, [isAuthenticated]);
+
+useEffect(() => {
+  if (isOnDesktop === false) {
+      setSidebarOpen(false);
+  }
+}, [isOnDesktop]);
+
+console.log(isAuthenticated)
+
+if (!isAuthenticated) {
+    return <p>Você deve estar logado para visualizar essa página</p>;
+}
   return (
     <G.Container style={{ display: 'flex' }}>
-      <Sidebar />
+      {sidebarOpen && <Sidebar />}
+
+      {isOnDesktop === false && (
+                <div onClick={() => setSidebarOpen(!sidebarOpen)} style={{ height: 30 }}>
+                    <FiMenu style={{ width: 30, height: 30 }} />
+                </div>
+            )}
       <S.ContainerHome style={{ display: 'flex', justifyContent: 'center', width: '100%', height: '100%' }}>
         {/* Card 1 - Home */}
         <S.WelcomeCard>
@@ -45,17 +101,25 @@ export const Home = () => {
 
         {/* Card 3 - Bem-vindo à KYOPS! */}
         <S.WelcomeCard>
-          <S.WelcomeTitle><PiNewspaperClippingFill style={{ marginRight: 10 }} />Bem-vindo à KYOPS!</S.WelcomeTitle>
+          <S.WelcomeTitle><PiNewspaperClippingFill style={{ marginRight: 10 }} />Sair!</S.WelcomeTitle>
           <S.PhotosAvatar>
             <img src={Avatar1} alt="" />
             <img src={Avatar2} alt="" />
             <img src={Avatar3} alt="" />
           </S.PhotosAvatar>
           <S.WelcomeMessage>
-            <p>Aproveite para conhecer um pouco mais e interagir com outros usuários em nossa plataforma?</p>
+            <p>Deixar a plataforma</p>
             <Button Text="Conheça" BackgroundColor="transparent" Border="1px solid #fff" />
           </S.WelcomeMessage>
         </S.WelcomeCard>
+
+        <S.WelcomeCard>
+          <S.WelcomeTitle><PiNewspaperClippingFill style={{ marginRight: 10 }} />Acesse seu perfil</S.WelcomeTitle>
+          <img src={Avatar1} alt="" />
+          <S.ModernMessage>Clique aqui para acessar seu perfil</S.ModernMessage>
+          <Button Text="Acessar" BackgroundColor="transparent" Border="1px solid #fff" />
+        </S.WelcomeCard>
+
 
       </S.ContainerHome>
     </G.Container>
--- src/pages/Home/styles.jsx
+++ src/pages/Home/styles.jsx
@@ -52,3 +52,39 @@ export const WelcomeMessage = styled.p`
   font-size: 16px;
   margin-top: 20px;
 `;
+
+export const ModernCard = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  width: 300px;
+  height: 300px;
+  background: #2e2e2e; /* Dark background color */
+  color: #fff; /* Text color */
+  text-align: center;
+  padding: 20px;
+  border-radius: 12px;
+  box-shadow: 0 4px 12px rgba(0, 0, 0, 0.2);
+  margin: 15px 5px;
+
+  img {
+    width: 100px;
+    height: 100px;
+    border-radius: 50%;
+    border: 5px solid #2e2e2e; /* Same as the background color */
+    margin-bottom: 15px;
+  }
+`;
+
+export const ModernTitle = styled.h1`
+  font-size: 24px;
+  margin-bottom: 10px;
+`;
+
+export const ModernMessage = styled.p`
+  font-size: 16px;
+  margin-top: 20px;
+`;
+
+
